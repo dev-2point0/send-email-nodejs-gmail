f0ebbab904e62de7b719fd78cecf4e7aeb705479
Sintu Singh <sintu.singh@Sintus-MacBook-Pro.local>
2022-02-14 11:27:33 -0500

    Change route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { emailController } = require('./email.js');
 const app = express();
 const port = 3000;
-app.get('/email', emailController);
+app.get('/', emailController);
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
